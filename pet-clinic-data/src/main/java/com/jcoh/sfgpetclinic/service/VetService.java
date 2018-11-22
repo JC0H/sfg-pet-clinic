b8fde8b00f41c33ac0a952f08576863257989c58
Andriy Androsyuk <andriiandrosiukk@gmail.com>
2018-11-22 20:38:03 +0100

    Implement interfaces for serveces. closes #5

---
+++ pet-clinic-data/src/main/java/com/jcoh/sfgpetclinic/service/VetService.java
@@ -0,0 +1,14 @@
+package com.jcoh.sfgpetclinic.service;
+
+import com.jcoh.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
