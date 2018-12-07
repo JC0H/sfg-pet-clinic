45c131905047de33b806a3e877e63cff57808198
Andriy Androsyuk <andriiandrosiukk@gmail.com>
2018-12-07 13:16:45 +0100

    Add Controller which list all vets. closes #16

--- pet-clinic-web/src/main/java/com/jcoh/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/jcoh/sfgpetclinic/controller/VetController.java
@@ -1,13 +1,23 @@
 package com.jcoh.sfgpetclinic.controller;
 
+import com.jcoh.sfgpetclinic.service.map.VetMapService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetMapService vetMapService;
+
+    public VetController(VetMapService vetMapService) {
+        this.vetMapService = vetMapService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String showVetList(){
+    public String showVetList(Model model){
+        model.addAttribute("vets", vetMapService.findAll());
+
         return "vets/index";
     }
 }
