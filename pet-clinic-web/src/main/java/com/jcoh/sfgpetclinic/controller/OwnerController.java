0755fdb881b8e56a0bfa19cf7a16c27712e04ad7
Andriy Androsyuk <andriiandrosiukk@gmail.com>
2018-12-07 12:51:13 +0100

    Add Controller which list all owners. closes #23

--- pet-clinic-web/src/main/java/com/jcoh/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/jcoh/sfgpetclinic/controller/OwnerController.java
@@ -1,14 +1,24 @@
 package com.jcoh.sfgpetclinic.controller;
 
+import com.jcoh.sfgpetclinic.service.map.OwnerMapService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerMapService ownerMapService;
+
+    public OwnerController(OwnerMapService ownerMapService) {
+        this.ownerMapService = ownerMapService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String showVetList(){
+    public String showVetList(Model model){
+        model.addAttribute("owners", ownerMapService.findAll());
+
         return "owners/index";
     }
 }
